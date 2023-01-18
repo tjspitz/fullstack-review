6bb859bcf75945d20aece7614aac48d2f3ac7ea0
Tom Spitz <tjspitz1986@gmail.com>
2023-01-17 19:29:18 -0600

    'Fixed' search fn; have axios post req built; have ajax post req built, hopefully correctly.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,13 +4,39 @@ import $ from 'jquery';
 import Search from './components/Search.jsx';
 import RepoList from './components/RepoList.jsx';
 
+import axios from 'axios';
+
+
 const App = () => {
 
   const [repos, setRepos] = useState([]);
 
   const search = (term) => {
+    // this is where I send am AJAX POST req to
+    // endpoint is /repos
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:1128/repos',
+      data: term,
+      dataType: 'json',
+      success: (req) => {
+        console.log('Success! AJAX is a pain!', req);
+      },
+      error: (jqXHR, textStatus) => {
+        let res = $.parseJSON(jqXHR.responseText);
+        console.log('Booo, an error:', res);
+      }
+    });
+
+
+    // axios.post('/repos', {
+    //   searchTerm: term
+    // })
+    // .then(() => { console.log('axios post sent') } )
+    // .catch(() => { console.log('axios post ERROR') } )
+
     console.log(`${term} was searched`);
-  }
+  };
 
   return (
     <div>
