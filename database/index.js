6a904568af3a9fd418ed3a78815d21f4f2c22061
Tom Spitz <tjspitz1986@gmail.com>
2023-01-17 19:32:34 -0600

    Built prelim Schema, need to figure out 'save' method.

--- database/index.js
+++ database/index.js
@@ -1,8 +1,26 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+/*
+repo._id ------ mongoose-provided, unique, auto-incrementing | NUM
+repo.owner.id --- specific to user | NUM
+repo.full_name --- to display of 1) username, 2) repo name | TEXT
+repo.id ------ user's specific repo ID --- filtering, maybe | NUMBER
+repo.owner.avatar_url ------ to make display more pretty? | TEXT
+repo.html_url ------ provide link to actual repo? | TEXT
+repo.forks_count ------ filtering, maybe | NUM
+repo.watchers_count ------ filtering, maybe | NUM
+*/
+
+// By default, Mongoose adds an _id property to your schemas.
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  userId: Number,
+  fullName: String,
+  repoId: Number,
+  pic: String,
+  link: String,
+  forks: Number,
+  watchers: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
@@ -11,6 +29,7 @@ let save = (/* TODO */) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+
 }
 
 module.exports.save = save;
