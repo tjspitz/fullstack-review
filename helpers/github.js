19e172ea4f6c44876ae8a8bd6f4c273d7ac78299
Tom Spitz <tjspitz1986@gmail.com>
2023-01-19 10:42:59 -0600

    Is quite possibly functioning correctly now.

--- helpers/github.js
+++ helpers/github.js
@@ -1,24 +1,31 @@
 const axios = require('axios');
 const config = require('../config.js');
 
-let getReposByUsername = (term) => {
-  // TODO - Use the axios module to request repos for a specific
-  // user from the github API
+let getReposByUsername = (searchTerm) => {
 
-  // The options object has been provided to help you out,
-  // but you'll have to fill in the URL
   let options = {
-    url: `https://api.github.com/users/${term}/repos`,
+    url: `https://api.github.com/users/${searchTerm}/repos`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
 
-  axios.get(options.url, options)
-    .then(response => console.log('github.js response data: ', response.data))
-    .catch(error => console.log('github.js error: ', error));
+  // RETURN the async request, data, etc.
+  return axios.get(options.url, options)
+    .then(repos => {
+      console.log('\nSUCCESS in helpers/github.js!')
+      return repos;
+    })
+    .catch(error => console.log('\nERROR in helperds/github.js: ', error))
 
-}
+  // SOLUTION VID DOES NOT USE AXIOS...?
+  // request(options, (err, response, body) => {
+  //   body = JSON.parse(body);
+  //   if (err) console.log(err);
+  //   else callback(body)
+  // })
+
+};
 
 module.exports.getReposByUsername = getReposByUsername;
