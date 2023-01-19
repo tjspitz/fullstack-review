e3936272d281ee8c3162ee5206568bc7b2fe6a34
Tom Spitz <tjspitz1986@gmail.com>
2023-01-19 10:41:35 -0600

    Seem to be hitting the database correctly. Yet to verify sucessful saving though.

--- server/index.js
+++ server/index.js
@@ -1,38 +1,50 @@
 const express = require('express');
+const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const morgan = require('morgan');
+const db = require('../database')
+const gitHelp = require('../helpers/github.js');
 
-const getReposByUsername = require('../helpers/github.js');
-
-
-// TODO - your code here!
-// Set up static file service for files in the `client/dist` directory.
-// Webpack is configured to generate files in that directory and
-// this server must serve those files when requested.
 app.use(morgan('dev'))
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.post('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should take the github username provided
-  // and get the repo information from the github API, then
-  // save the repo information in the database
 
-  // the req is an obj, the req.body is too, go to the prop whose val is...
-  // a string!
-  
-  getReposByUsername.getReposByUsername(req.body.searchTerm);
+  // ================= SOLUTION VID IDEAS =================
 
-  console.log(`POST in server/index.js: ${req.body.searchTerm}`);
-  // console.log('res: ', res);
+  // gitHelp.getReposByUsername(req.body.searchTerm)
+    // db.save(data)
+    // .then( () => res.sendStatus(201))
+    // .then((data) => db.save(data))
+
+  // ================= PRE-VID IDEAS =================
+
+  gitHelp.getReposByUsername(req.body.searchTerm)
+    .then(repos => {
+      // console.log('response in THEN of server/index.js: ', response)
+      db.save(repos.data)
+      console.log('\nSUCCES in server/index.js! ');
+    })
+    .then(res => res)
+    .catch(err => console.log('\nERROR in server/index.js: ', err))
+
 });
 
 app.get('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should send back the top 25 repos
+
+// ================= SOLUTION VID IDEAS =================
+  // db.find({})
+  //   .sort('-forks')
+  //   .limit(25)
+  //   .exec()
+  //   .then((repos) => {
+  //     res.send(repos);
+  //   })
+  //   .catch((err) => {
+  //     console.log('Error querying mongoBD!');
+  //   })
 
 
   console.log(`GET in server/index.js: ${req.body}`);
